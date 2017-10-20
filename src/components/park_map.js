4b522487b444c16dd9d6fa0e6c8a735f2a446eeb
isabel cusick <cusick.isabel@gmail.com>
2017-10-20 15:49:28 -0500

    nps api up and running. starts making ajax requests

--- src/components/park_map.js
+++ src/components/park_map.js
@@ -1,1 +1,32 @@
-park_map.js
+// import React, { Component } from 'react';
+// import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+
+// const style = {
+// 	width: '300px', 
+// 	height: '300px'
+// }
+
+// class GoogleMap extends Component {
+
+
+// 	render() {
+// 		return (
+			
+// 			<Map 
+// 				google={this.props.google} 
+// 				zoom={12}
+// 				style={style}
+// 				initialCenter={{
+//             		lat:  this.props.lat,
+//             		lng:  this.props.lon 
+//           		}}
+// 			></Map>
+			
+
+// 		);
+// 	}
+// }
+
+// export default GoogleApiWrapper({
+//   apiKey: 'AIzaSyBisoAT_PP5ybV9UXfIsVktOVF4jopAYgg'
+// })(GoogleMap);
--- src/components/park_show.js
+++ src/components/park_show.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { Carousel } from 'react-responsive-carousel';
 import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 import ParkWeather from './park_weather';
+import GoogleMap from './park_map';
 
+const NPS_API_KEY = 'B10fQSv2VLNENYG0DViy5qrHdRNSnl3vh1IQpeF1';
+const NPS_ROOT_URL = '//developer.nps.gov/api/v1/parks?parkCode=';
 
 const ParkAPI = {
   parks: [
-    { id: 1, name: "Acadia National Park", location: "Maine", images: ['../images/acadia_1.jpg', '../images/acadia_2.jpg', '../images/acadia_3.jpg'] },
-    { id: 2, name: "Yosemite", location: "California", images: ['../images/acadia_1.jpg', '../images/acadia_2.jpg', '../images/acadia_3.jpg'] },
-    { id: 3, name: "Yellowstone National Park ", location: "Wyoming", images: ['../images/yellowstone3.jpg', '../images/yellowstone3.jpg', '../images/yellowstone4.jpg'] },
-    { id: 4, name: "Grand Teton National Park", location: "Wyoming", images: ['../images/grand_tetons1.jpg', '../images/grand_tetons_3.jpg', '../images/grand_tetons_6.jpg'] },
-    { id: 5, name: "Grand Canyon National Park", location: "Arizona", images: ['../images/grand_canyon_1.jpg', '../images/grand_canyon_2.jpg', '../images/grand_canyon_3.jpg'] },
-    { id: 6, name: "Mesa Verde National Park", location: "Colorado", images: ['../images/', '../images/', '../images/'] }
+    { id: 1, parkCode: "acad", name: "Acadia National Park", location: "Maine", images: ['../images/acadia_1.jpg', '../images/acadia_2.jpg', '../images/acadia_3.jpg'] },
+    { id: 2, parkCode: "yose", name: "Yosemite", location: "California", images: ['../images/acadia_1.jpg', '../images/acadia_2.jpg', '../images/acadia_3.jpg'] },
+    { id: 3, parkCode: "yell", name: "Yellowstone National Park ", location: "Wyoming", images: ['../images/yellowstone3.jpg', '../images/yellowstone3.jpg', '../images/yellowstone4.jpg'] },
+    { id: 4, parkCode: "acad", name: "Grand Teton National Park", location: "Wyoming", images: ['../images/grand_tetons1.jpg', '../images/grand_tetons_3.jpg', '../images/grand_tetons_6.jpg'] },
+    { id: 5, parkCode: "acad", name: "Grand Canyon National Park", location: "Arizona", images: ['../images/grand_canyon_1.jpg', '../images/grand_canyon_2.jpg', '../images/grand_canyon_3.jpg'] },
+    { id: 6, parkCode: "acad", name: "Mesa Verde National Park", location: "Colorado", images: ['../images/', '../images/', '../images/'] }
   ],
   all: function() { return this.parks},
   get: function(id) {
@@ -22,13 +26,32 @@ const ParkAPI = {
   }
 }
 
-
-
 class ParkShow extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			description: ""
+		};
+	}
+
+	componentDidMount() {
+		axios.get(`${NPS_ROOT_URL}yell&api_key=${NPS_API_KEY}`)
+			.then(response => {
+				console.log(response);
+				console.log(response.data);
+				console.log(response.data.data);
+				console.log(response.data.data.map(obj => obj.description));
+				const description = response.data.data.map(obj => obj.description);
+				// console.log(description);
+				this.setState({ description: description });
+			});
+	}
+
 	render() {
 		const park = ParkAPI.get(parseInt(this.props.match.params.id, 10));
 
-		console.log(this.props);
+		// console.log(this.props);
   		
   		if (!park) {
   			return <div>Sorry, the park was not found</div>
@@ -53,7 +76,9 @@ class ParkShow extends Component {
       					</div>
       			</Carousel>
       			</div>
+      			<p>{this.state.description}</p>
       			<ParkWeather city={park.name} />
+      			
     		</div>
 		)
   	}
