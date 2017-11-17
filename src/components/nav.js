96d2a667ca0c0e69ffd13675f8fd92c199992e72
isabel cusick <cusick.isabel@gmail.com>
2017-11-17 16:29:39 -0600

    adds dropdown

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { ParkAPI } from './parks_index';
+import { Link } from 'react-router-dom';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
 
 class Nav extends Component {
 	render() {
@@ -7,10 +10,17 @@ class Nav extends Component {
 		        <a className="navbar-brand" href="/">
 		          <img className="d-inline-block align-top" src="../images/mountain_4.png" width="40" height="40" alt=""></img>
 		        </a>	
-		        <h2 className="offset-md-1">ParkDash</h2>	        
+		        <h2 className="offset-md-1">ParkDash</h2>	
+		        <DropdownButton bsStyle="default" title="Pick a park" key="1" id={`dropdown-basic-{1}`}>
+		        {
+            ParkAPI.all().map(p => (
+              <MenuItem><Link to={`/parks/${p.id}`}><p>{p.name}</p></Link></MenuItem>
+              ))
+            }       </DropdownButton>
       	</nav>		
 	)}	
 
 }
 
 export default Nav;
+
